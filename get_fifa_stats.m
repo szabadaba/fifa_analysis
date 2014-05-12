c992f336750d4592d77e256d54e30ad095a26ee1
Steven Szabados <szabados@mybasis.com>
2014-05-12 11:53:58 -0700

    add files

---
+++ get_fifa_stats.m
@@ -0,0 +1,68 @@
+function [ results ] = get_fifa_stats( file_name )
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+
+c = csv2cell(file_name,'fromfile');
+
+team_list = {};
+goals_vs_mtx = [];
+numb_games = [];
+
+for i = 2:length(c)
+    %copy team names
+    home_team = c{i,2};
+    away_team = c{i,3};
+    
+    %do we need to add the team to the list?
+    if isempty(find(strcmp(team_list,home_team)))
+        team_list{length(team_list)+1} = home_team;
+        goals_vs_mtx(end+1, end+1) = 0;
+        numb_games = [numb_games; 0];
+    end
+    if isempty(find(strcmp(team_list,away_team)))
+        team_list{length(team_list)+1} = away_team;
+        goals_vs_mtx(end+1, end+1) = 0;
+        numb_games = [numb_games; 0];
+    end
+    
+    %fine indeces for teams
+    home_idx = find(strcmp(team_list,home_team));
+    away_idx = find(strcmp(team_list,away_team));
+    
+    %add goals for home team
+    goals_vs_mtx(home_idx, away_idx) = goals_vs_mtx(home_idx, away_idx) + str2num(c{i,4})*.75;
+    goals_vs_mtx(away_idx, home_idx) = goals_vs_mtx(away_idx, home_idx) + str2num(c{i,5});
+    
+    %add games
+    numb_games(home_idx) = numb_games(home_idx) + 1;
+    numb_games(away_idx) = numb_games(away_idx) + 1;
+    
+end
+
+idx_remove = find(numb_games < 30);
+
+goals_vs_mtx(idx_remove,:) = [];
+goals_vs_mtx(:,idx_remove) = [];
+numb_games(idx_remove) = [];
+
+
+SCALE_M = diag(1./numb_games);
+[e_vect, ~] = eig(SCALE_M*goals_vs_mtx);
+
+team_rank = abs(e_vect(:,1));
+
+
+[x, I] = sort(team_rank);
+results.Ranked_Teams = team_list(I);
+
+[x, I] = sort(sum(goals_vs_mtx')./numb_games');
+results.Rank_By_Goals = team_list(I);
+
+figure;
+plot(team_rank);
+
+results.team_list = team_list;
+results.goals_vs_mtx = goals_vs_mtx;
+results.numb_games = numb_games;
+
